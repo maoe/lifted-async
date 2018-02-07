55bce0cc9f9026269240df92159cb6155296464e
Mitsutoshi Aoe <maoe@foldr.in>
2018-02-07 15:28:36 +0900

    Update copyright years

--- src/Control/Concurrent/Async/Lifted.hs
+++ src/Control/Concurrent/Async/Lifted.hs
@@ -6,7 +6,7 @@
 
 {- |
 Module      : Control.Concurrent.Async.Lifted
-Copyright   : Copyright (C) 2012-2017 Mitsutoshi Aoe
+Copyright   : Copyright (C) 2012-2018 Mitsutoshi Aoe
 License     : BSD-style (see the file LICENSE)
 Maintainer  : Mitsutoshi Aoe <maoe@foldr.in>
 Stability   : experimental
--- src/Control/Concurrent/Async/Lifted/Safe.hs
+++ src/Control/Concurrent/Async/Lifted/Safe.hs
@@ -11,7 +11,7 @@
 
 {- |
 Module      : Control.Concurrent.Async.Lifted.Safe
-Copyright   : Copyright (C) 2012-2017 Mitsutoshi Aoe
+Copyright   : Copyright (C) 2012-2018 Mitsutoshi Aoe
 License     : BSD-style (see the file LICENSE)
 Maintainer  : Mitsutoshi Aoe <maoe@foldr.in>
 Stability   : experimental
